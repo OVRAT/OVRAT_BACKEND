a2f85b1721f20e0e4ea441e9169b12a19de4c9c7
danieljou <danielsitepro@gmail.com>
2023-07-03 13:54:47 +0200

    authentication and users creation

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 # Create your views here.
+from .serializers import *
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializers
+    queryset = get_user_model().objects.all()
